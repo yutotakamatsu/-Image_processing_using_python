f884a92377eb3cef237768f6a41b22234aba54f3
yutotakamatsu <siburin0509@gmail.com>
2020-12-01 08:14:20 +0900

    Fix list4.py

--- list4.py
+++ list4.py
@@ -1,20 +1,31 @@
+import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-fps = 30
 
-size = (640, 480)
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+# Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-video = cv2.VideoWriter('output.avi', fourcc, fps, size)
+#out = cv2.VideoWriter('../desktop/output.avi',fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('movie/output.mp4',fourcc, fps, (width,height))
 
-while (cap.isOpened()):
+while(cap.isOpened()):
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    video.write(frame)
-            
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if ret==True:
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
-        cap.release()
-        video.release()
-        cv2.destroyAllWindows()
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
